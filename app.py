546c2f12ce53c20d45936bffbed70c2a3592ca92
Madhun13 <nainmadhu1316@gmail.com>
2025-12-07 17:51:24 +0530

    Add Untitled5 notebook

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import joblib
 model = joblib.load("kmeans_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
-st.title(" Customer Segmentation RFM Clustering")
+st.title(" Customer Segmentation")
 
 st.write("Enter RFM values and get the customer segment")
 
 # user inputs
 r = st.number_input("Recency (days)", min_value=0)
 f = st.number_input("Frequency", min_value=0)
-m = st.number_input("Monetary Value", min_value=0)
+m = st.number_input("Money Value", min_value=0)
 
 if st.button("Predict"):
     df = pd.DataFrame([[r, f, m]], columns=["Recency","Frequency","Monetary"])
